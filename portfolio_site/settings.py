041abc0303ac4a78692bb6e041b52d00377b7bcd
Gokul7625 <kannansaravanan09@gmail.com>
2025-07-29 18:04:02 +0530

    Fix: Added collectstatic and staticfiles for deployment

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,3 +129,7 @@ import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio_app/static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
